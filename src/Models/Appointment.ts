a62165142a92455beaacd6284dcee4c2471216c9
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-06-23 19:24:40 -0300

    Criando model de agendamento

--- src/Models/Appointment.ts
+++ src/Models/Appointment.ts
@@ -1,10 +1,14 @@
 import { uuid } from 'uuidv4';
-
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column() // se não colocar nada o padrão é varchar
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
 
   constructor({ provider, date }: Omit<Appointment, 'id'>) {
