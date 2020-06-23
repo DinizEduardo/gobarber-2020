de0c86336b6e2403ebc51bea9ce3bc79f5f4ae44
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-06-23 17:53:33 -0300

    Commit inicial

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,37 @@
+import Appointment from '../Models/Appointment';
+import { isEqual } from 'date-fns';
+
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentRepository;
