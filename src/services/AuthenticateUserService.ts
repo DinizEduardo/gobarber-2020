d2b0d58e333c90a90cd8418b3868a80d5e2b10d7
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-06-24 20:24:17 -0300

    Gerando token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../Models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect Email/Password combination!');
     }
 
-    return { user };
+    const token = sign({}, '935ae28250c5dc4de0a38cd95636093a', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
