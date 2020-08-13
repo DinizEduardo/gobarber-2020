69b765518353ca7b032b0620b493366489070a01
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-08-12 22:01:56 -0300

    template engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-
+import path from 'path';
 import { injectable, inject } from 'tsyringe';
 
 import User from '../infra/typeorm/entities/User';
@@ -34,6 +34,8 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -41,10 +43,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Recuperação de senha',
       templateDate: {
-        template: 'Olá, {{name}}: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token
+          link: `http://localhost:3000/reset_password?token=${token}`
         }
       }
     }
