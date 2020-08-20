7456f313bf956420420d0b8b7474bfce6352410d
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-08-20 13:44:16 -0300

    Utilizando class transform

--- src/modules/users/infra/http/controllers/ProfileController.ts
+++ src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
@@ -16,9 +16,7 @@ export default class ProfileController {
 
     const user = await showProfile.execute({ user_id });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -30,8 +28,6 @@ export default class ProfileController {
 
     const user = await updateUser.execute({ user_id, name, email, password, old_password });
 
-    delete user.password;
-
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
