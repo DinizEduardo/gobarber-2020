0d7bae0bb7fedfcfd321c1333c9f83c822f1a7d6
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-08-25 18:14:29 -0300

    Invalidando o cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -24,13 +24,11 @@ class ListProviderAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
-    @inject('CacheProvider')
-    private cacheProvider: ICacheProvider
   ) { }
 
   public async execute({ provider_id, day, month, year }: Request): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asdas');
-    console.log(cacheData);
+    // const cacheData = await this.cacheProvider.recover('asdas');
+    // console.log(cacheData);
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       {
         provider_id,
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 
 import { injectable, inject } from 'tsyringe';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '../infra/typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
@@ -21,7 +22,9 @@ class CreateUserService {
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
     @inject('HashProvider')
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) { }
 
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -38,6 +41,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
