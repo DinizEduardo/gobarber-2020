5c479eb2712b8c5eb846f7632b6ea8b8bb544971
Eduardo Diniz <eduardo.diniz987@gmail.com>
2020-08-31 15:00:44 -0300

    Agendamentos no mesmo horario

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -36,9 +36,10 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
   }
 
 
-  public async findByDate(date: Date): Promise<Appointment | undefined> {
+  public async findByDate(date: Date, provider_id: string): Promise<Appointment | undefined> {
     const findAppointment = this.appointments.find(appointments =>
-      isEqual(appointments.date, date)
+      isEqual(appointments.date, date) &&
+      appointments.provider_id === provider_id
     );
 
     return findAppointment;
